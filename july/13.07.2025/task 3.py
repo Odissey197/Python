ed2733a1f1f34cc060ea4cac5f57b35210c5eac0
Odissey <adikaxper@yandex.ru>
2025-07-13 20:53:27 +0300

    13.07.2025 magic method classmethod staticmethod

---
+++ july/13.07.2025/task 3.py
@@ -0,0 +1,30 @@
+class Wallet:
+    def __init__(self, count):
+        self.money = count
+
+    def __add__(self, value):
+        if isinstance(value, Wallet):
+            return Wallet(self.money + value.money)
+        elif isinstance(value, int):
+            return Wallet(self.money + value)
+        return NotImplemented
+    
+    def __radd__(self, value):
+        if isinstance(value, Wallet):
+            return Wallet(self.money + value.money)
+        return NotImplemented
+        
+    def __str__(self):
+        return f"Баланс кошелька = {self.money}"
+        
+w1 = Wallet(100)
+w2 = Wallet(500)
+w3 = Wallet(w1 + w2)
+
+print(w1)
+print(w2)
+print(w3)
+
+w1 = 5 + w1
+w1 = w1 + 5 # w1.
+print(w1)

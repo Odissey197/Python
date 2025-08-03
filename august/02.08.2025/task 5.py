327449f47542392edf205a7effdbf16e405db64a
Odissey <adikaxper@yandex.ru>
2025-08-03 20:49:26 +0300

    Завершение упаковки данных 03.08.2025

--- august/02.08.2025/task 5.py
+++ august/02.08.2025/task 5.py
@@ -1,26 +1,48 @@
+import json
+
+file_name = ''
 
 musicians = {
-    "Linlin Park": ["Hybrid Theory", "One More Light"],
+    "Linkin Park": ["Hybrid Theory", "One More Light"],
     "Rammstein": ["Mutter", "Zeit"],
     "Miyagi": ["Yamakasi", "Buster Keaton"]
 }
 
 def show_menu():
     print("Выберете операцию: ")
+    print("0 - Загрузить данные из файла")
     print("1 - Выбрать группу")
     print("2 - Добавить группу")
     print("3 - Удалить группу")
-    print("4 - Выход")
+    print("4 - Сохранить изменения")
+    print("5 - Выход")
 
 while True:
     show_menu()
     choice = int(input())
-    if choice == 1:
+
+    if choice == 0:
+        file_name = input("Введите название файла для загрузки: \n")
+        try:
+            with open(file_name, "r", encoding="utf-8") as fl:
+                musiciansn = json.load(fl)
+                print(f"Файл с именем {file_name} успешно загружен!")
+        except FileNotFoundError:
+            answer = input("Файл не найден, создать пустой каталог? (Да/Нет): \n")
+            if answer.lower().strip() == 'да':
+                with open(file_name, "w", encoding="utf-8") as fl:
+                    json.dump(musicians, fl)
+                print(f"Файл с именем {file_name} успешно создан!")
+        except Exception as e:
+            print("Произошла непредвиденная ошибка: {e}")
+
+    elif choice == 1:
         group = input("Введите название группы: ")
         if musicians.get("group_for_add", None) is not None:
-            print(musicians.get("group_for_add", None))
+            print(musicians.get(group, None))
         else:
-            print(f"Группы {group_for_add} нет в каталоге!")
+            print(f"Группы {group} нет в каталоге!")
+
     elif choice == 2:
         group_for_add = input("Введите название группы для добавления: ")
         if musicians.get("group_for_add", None) is not None:
@@ -28,6 +50,15 @@ while True:
         else:
             musicians[group_for_add] = []
             print(f"Группа {group_for_add} добавлена в каталог")
+            answer = input("Хотите добавить альбомы к группе? (Да/Нет): ")
+            if answer.lower().strip() == 'да':
+                albums = input("Введите одно или несколько названий альбомов через запятую: \n")
+                if albums.isspace():
+                    print("Строка пуста, альбом(-ы) не добавлен(-ы)")
+                else:
+                    musicians[group_for_add] = [x.strip().capitalize() for x in albums.split(',')]
+                    print(f"Для {group_for_add} добавлены альбомы: {musicians[group_for_add]}")
+
     elif choice == 3:
         group_for_del = input("Введите название группы для удаления: ")
         try:
@@ -35,7 +66,21 @@ while True:
             print(f"Группа {group_for_del} успешно удалена")
         except:
             print("Такой группы нет в каталоге")
+
     elif choice == 4:
+        if not file_name:
+            answer = input("Не выбран файл для сохранения, создать новый файл? (Да/Нет): ")
+            if answer.lower().strip() == 'да':
+                file_name = input("Введите название файла для сохранения: \n")
+            else:
+                print("Изменения не сохранены!")
+                continue
+
+        with open(file_name, "w", encoding="utf-8") as fl:
+            json.dump(musicians, fl)
+        print(f"Все изменения успешно сохранены в {file_name}!")
+
+    elif choice == 5:
         print("Выходим из программы")
         break
     else:
